41e539d4849b211a19bc9cdbd76adc76d4bcd4f3
valttku <valtteri.kuitula@metropolia.fi>
2024-02-27 11:09:50 +0200

    first commit

---
+++ src/types/DBTypes.ts
@@ -0,0 +1,43 @@
+import {Document, Types} from 'mongoose';
+
+type User = Partial<Document> & {
+  id: Types.ObjectId | string;
+  user_name: string;
+  email: string;
+  role: 'user' | 'admin';
+  password: string;
+};
+
+type UserOutput = Omit<User, 'password' | 'role'>;
+
+type UserInput = Omit<User, 'id' | 'role'>;
+
+type UserTest = Partial<User>;
+
+type LoginUser = Omit<User, 'password'>;
+
+type TokenContent = {
+  token: string;
+  user: LoginUser;
+};
+
+type Location = {
+  lat: number;
+  lng: number;
+};
+
+type LocationInput = {
+  topRight: Location;
+  bottomLeft: Location;
+};
+
+export {
+  User,
+  UserOutput,
+  UserInput,
+  UserTest,
+  LoginUser,
+  TokenContent,
+  Location,
+  LocationInput,
+};
